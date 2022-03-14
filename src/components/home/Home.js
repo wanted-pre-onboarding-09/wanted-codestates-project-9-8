49920312c93d6d7dfa7021319e7c2145732ecb23
ysh2987 <ysh2987@naver.com>
2022-03-14 20:29:30 +0900

    #5 Feat: home list

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { AiOutlinePlusCircle } from 'react-icons/ai';
+import styled from 'styled-components';
+import Header from './Header';
+import FormList from './FormList';
+// import axios from 'axios';
 
 function Home() {
   return (
-    <div>
-      <p>홈</p>
-    </div>
+    <>
+      <Header />
+      <FormList />
+      <StyledPulsBtn>
+        <AiOutlinePlusCircle size={50} fill="#fa4646" cursor="pointer" />
+      </StyledPulsBtn>
+    </>
   );
 }
 
 export default Home;
+
+const StyledPulsBtn = styled.div`
+  text-align: center;
+  margin-top: 30px;
+`;
