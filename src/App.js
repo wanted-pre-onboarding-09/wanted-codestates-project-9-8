66b397c744fad3a247f5813f8dc6b5feae7bf87a
ysh2987 <ysh2987@naver.com>
2022-03-14 15:28:18 +0900

    #3 Feat: main layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
+import Home from './components/home/Home';
+import List from './components/list/List';
 
 function App() {
-  return <p>홈</p>;
+  return (
+    <StyledContainer>
+      <StyledWrap>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/list" element={<List />} />
+        </Routes>
+      </StyledWrap>
+    </StyledContainer>
+  );
 }
 
 export default App;
+
+const StyledContainer = styled.main`
+  padding: 20px 0;
+`;
+
+const StyledWrap = styled.div`
+  width: 380px;
+  height: auto;
+  min-height: 100vh;
+  margin: 0 auto;
+`;
