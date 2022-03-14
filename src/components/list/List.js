abcaf13e9507a8f1df6124bee3b41f0a9c3f15e6
y-solb <ysolb99@gmail.com>
2022-03-14 22:50:42 +0900

    Feat: Add Modal to List component

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import ListCard from './ListCard';
+import Modal from '../modal/Modal';
 
 function List() {
   const [getData, setGetData] = useState([]);
+
+  const [isModal, setIsModal] = useState(false);
+  const [modalData, setModalData] = useState();
+
+  const handleModal = (data) => {
+    setIsModal(!isModal);
+    setModalData(data);
+  };
+
+  const handleClose = () => {
+    setIsModal(false);
+  };
 
   useEffect(async () => {
     const { data } = await axios.get(
@@ -21,8 +34,10 @@ function List() {
           name={item.fcNm}
           addres={item.fcAddr}
           phone={item.ref1}
+          handleModal={() => handleModal(item)}
         />
       ))}
+      {isModal && <Modal handleClose={handleClose} data={modalData} />}
     </div>
   );
 }
