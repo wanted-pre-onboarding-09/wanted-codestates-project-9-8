a5ff412eba65d645cdbd15d183ba05234ed08e5d
jisoo <devjisoolee@gmail.com>
2022-03-14 18:05:59 +0900

    feat: toast component implement

---
+++ src/components/common/Toast.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import styled, { css } from 'styled-components';
+import PropTypes from 'prop-types';
+
+// eslint-disable-next-line react/prop-types
+function Toast({ type }) {
+  const [isFade, setIsFade] = useState(false);
+  const [msg, setMsg] = useState('');
+
+  useEffect(() => {
+    switch (type) {
+      case 'change':
+        setMsg({ type: 'change', message: '수정이 완료되었습니다' });
+        break;
+      case 'add':
+        setMsg({ type: 'add', message: '작성이 완료되었습니다' });
+        break;
+      case 'delete':
+        setMsg({ type: 'delete', message: '삭제가 완료되었습니다' });
+        break;
+      case 'warning':
+        setMsg({ type: 'warning', message: '내용을 입력해주세요' });
+        break;
+
+      default:
+        break;
+    }
+  }, []);
+
+  //   setTimeout(() => {
+  //     setIsFade(true);
+  //     clearTimeout(setIsFade(true));
+  //   }, 2000);
+
+  return (
+    <StyledToast isFade={isFade} msg={msg}>
+      {msg.message}
+    </StyledToast>
+  );
+}
+const StyledToast = styled.div`
+  padding: 0.7rem 1rem;
+  width: fit-content;
+  border-radius: 0.8rem;
+  background-color: ${({ msg }) =>
+    msg.type === 'delete' || msg.type === 'warning' ? 'red' : 'green'};
+  color: white;
+  /* opacity: 0; */
+
+  ${({ isFade }) =>
+    isFade &&
+    css`
+      animation-name: slide;
+      animation-duration: 2s;
+      animation-timing-function: slideIn;
+    `}
+  @keyframes slide {
+    0% {
+      opacity: 1;
+      transform: translateX(100%);
+    }
+    50% {
+      opacity: 1;
+      transform: translateX(0%);
+    }
+    100% {
+      opacity: 0;
+      transform: translateX(100%);
+    }
+  }
+`;
+
+export default Toast;
+Toast.propsTypes = {
+  type: PropTypes.string,
+};
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Toast from '../common/Toast';
 
 function Home() {
-  return <p>홈</p>;
+  return (
+    <div>
+      <p>홈</p>
+      <Toast type="change" />
+    </div>
+  );
 }
 
 export default Home;
